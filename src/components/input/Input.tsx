4e98db9e1c89e1bf2655a5d8216f42deec5de5b1
Samukelo Khathi <sbkhathi005@gmail.com>
2026-08-19 10:59:39 +0200

    Added inputs also style them in login page

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -4,31 +4,35 @@ import inputStyle from "./Input.module.css";
 type InputProps = {
   id?: string;
   value?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
   style?: React.CSSProperties;
   label: string;
   error?: string;
   name?: string;
-  type?: string;
+  placeholder?: string;
+  type: React.HTMLInputTypeAttribute;
 };
 
 export const Input: React.FC<InputProps> = ({
   id,
   value,
+  onChange,
   style,
   label,
   error,
   name,
-  type,
+  type = "text",
 }) => {
   return (
     <div className={inputStyle["input-label-container"]}>
       <label className={inputStyle["input-label"]}>{label}</label>
       <input
         name={name}
-        type={type || "text"}
         id={id}
         style={style}
+        type={type}
         value={value}
+        onChange={onChange}
         className={inputStyle.input}
       />
       {error && <span className={inputStyle["input-error"]}>{error}</span>}
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,41 @@
-import React from "react";
 import { Input } from "../../components/input/Input";
-export default function Login() {
+import styles from "./Login.module.css";
+
+function Login() {
   return (
-    <div>
-      <Input label="Email" type="email" />
-      <Input label="Password" type="password" />
+    <div className={styles.page}>
+      <form className={styles.card}>
+        <Input
+          id="email"
+          name="email"
+          label="Email Address"
+          type="email"
+          value={"email"}
+          onChange={() => {}}
+        />
+
+        <Input
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={"password"}
+          onChange={() => {}}
+        />
+
+        <button className={styles.button} type="submit">
+          Sign in
+        </button>
+
+        <p className={styles.registerText}>
+          New here?{" "}
+          <button type="button" className={styles.link}>
+            Create an Account
+          </button>
+        </p>
+      </form>
     </div>
   );
 }
+
+export default Login;
