1b83da4581f90ab1cea40610420902bd60f6ce7b
samukelo <sbkhathi005@gmail.com>
2026-08-20 05:55:19 +0200

    Added signup slice functions

--- src/store/SignUpSlice.ts
+++ src/store/SignUpSlice.ts
@@ -1,32 +1,55 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
 interface SignInState {
     name: string,
     surname: string,
-    number: number,
-    email: string, 
-    password: string
+    number: string,
+    email: string,
+    password: string,
+    confirmPassword: string
 }
 
-const initialState : SignInState = {
+const initialState: SignInState = {
     name: "",
     surname: "",
-    number: 0,
+    number: "",
     email: "",
-    password: ""
+    password: "",
+    confirmPassword: ""
 }
 
-const SignInState = createSlice({
-    name: "signup page",
+const SignInSlice = createSlice({
+    name: "signupPage",
     initialState,
     reducers: {
+        setName(state, action: PayloadAction<string>) {
+            state.name = action.payload
+        },
+        setSurname(state, action: PayloadAction<string>) {
+            state.surname = action.payload
+        },
+        setNumber(state, action: PayloadAction<string>) {
+            state.number = action.payload
+        },
+        setEmail(state, action: PayloadAction<string>) {
+            state.email = action.payload
+        },
+        setPassword(state, action: PayloadAction<string>) {
+            state.password = action.payload
+        },
+        setConfirmPassword(state, action: PayloadAction<string>) {
+            state.confirmPassword = action.payload
+        },
+
+        resetForm(state) {
+            Object.assign(state, initialState)
+        }
+
 
     }
 })
- 
 
 
+export const { setName, setSurname, setNumber, setEmail, setPassword, setConfirmPassword, resetForm } = SignInSlice.actions
 
-
-
-export default SignInState.reducer
+export default SignInSlice.reducer
