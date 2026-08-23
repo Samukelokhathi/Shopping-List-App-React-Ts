d90864f1fb0cb272e80667da0e1a47d1ecd14220
samukelo <sbkhathi005@gmail.com>
2026-08-23 18:28:33 +0200

    Created home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Home from "./pages/Dashboard/Home";
 import { Route, Routes } from "react-router-dom";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
