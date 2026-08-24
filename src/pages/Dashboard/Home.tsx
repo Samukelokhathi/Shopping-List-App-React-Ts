fdbd48c8ae80c75006bed5ac578fbccd9210ec7f
samukelo <sbkhathi005@gmail.com>
2026-08-24 15:01:33 +0200

    Added hero section in home page

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,10 +1,37 @@
 import homeStyle from "../../pages/Dashboard/Home.module.css"
 import Navbar from "../../components/nav/NavBar";
 
+import Button from "../../components/Button/Button";
+import { Text } from "../../components/Text/Text";
+
 const Home = () => {
     return (
         <div className={homeStyle.container}>
             <Navbar />
+
+            <main className={homeStyle.main}>
+                <section className={homeStyle.hero}>
+                    <Text variant="p" className={homeStyle.greeting}>Hi User</Text>
+
+                    <Text variant="h1">Your shopping lists</Text>
+
+                    <Text variant="p" className={homeStyle.description}>
+                        Group items by category, track quantities and share a list
+                        with whoever is doing the shop.
+                    </Text>
+
+                    <Button
+                        className={homeStyle.newListButton}
+                        onClick={() => { }}
+                    >
+                        + New list
+                    </Button>
+                </section>
+
+
+            </main>
+
+
 
         </div>
     );
